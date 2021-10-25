c8c969909d03d483e16e8b0422a4ab4ce67fb501
Sadra Khosravi <sadrakh@outlook.com>
2021-10-24 23:12:58 -0700

    Fix CORS not accepting delete requests and restricited CORS access only to localhost:3000

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,12 +1,14 @@
 // Sets the CORS to only allow a specific URL to communicate with the API
 module.exports = (req, res, next) => {
   // Only support http://localhost:3000/
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+
   next();
   // If the browser is allowed to make the request
   if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     return res.status(200).json({});
   }
 };
